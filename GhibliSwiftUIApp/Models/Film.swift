0955e7a98ca000e5dc213a49644513791707bcaf
hp-code <phan.unity@gmail.com>
2025-10-28 11:42:57 +0700

    Add Json data

---
+++ GhibliSwiftUIApp/Models/Film.swift
@@ -0,0 +1,39 @@
+import Foundation
+import Playgrounds
+
+struct Film: Codable, Identifiable {
+    
+    let id: String
+    let title: String
+    let image: String
+    let description: String
+    let director: String
+    let producer: String
+    let people: [String]
+    
+    let bannerImage: String
+    let releaseYear: String
+    let duration: String
+    let score: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, image, description, director, producer, people
+        
+        case bannerImage = "movie_banner"
+        case releaseYear = "release_date"
+        case duration = "running_time"
+        case score = "rt_score"
+    }
+}
+
+#Playground {
+    let url = URL(string: "https://ghibliapi.vercel.app/films")!
+    do {
+        let (data, response) = try await URLSession.shared.data(from: url)
+        try
+        JSONDecoder().decode([Film].self, from: data)
+    } catch {
+        print(error)
+        
+    }
+}
