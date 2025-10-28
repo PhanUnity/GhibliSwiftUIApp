317a340400fba9fae38716902a59e14c8545bc25
hp-code <phan.unity@gmail.com>
2025-10-28 09:59:06 +0700

    Initial Commit

---
+++ GhibliSwiftUIApp/GhibliSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUIAppApp.swift
+//  GhibliSwiftUIApp
+//
+//  Created by HP on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GhibliSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GhibliSwiftUIAppTests/GhibliSwiftUIAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUIAppTests.swift
+//  GhibliSwiftUIAppTests
+//
+//  Created by HP on 28/10/25.
+//
+
+import Testing
+@testable import GhibliSwiftUIApp
+
+struct GhibliSwiftUIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
